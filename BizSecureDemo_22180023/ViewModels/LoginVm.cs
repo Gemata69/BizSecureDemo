22223c2d2402ef7d45e7877761f68fbeaba3aaea
Gemata69 <gemedjiev_69@abv.bg>
2026-03-03 18:08:01 +0200

    Add ViewModel classes for CreateOrder, Login, and Register
    Added `CreateOrderVm` with `Title` and `Amount` properties to
    represent order creation data. Introduced `LoginVm` with
    `Email` and `Password` for user authentication. Added
    `RegisterVm` with `Email` and `Password` for user registration,
    including validation rules for each property.

---
+++ BizSecureDemo_22180023/ViewModels/LoginVm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BizSecureDemo_22180023.ViewModels
+{
+    public class LoginVm
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+        [Required]
+        public string Password { get; set; } = "";
+    }
+}
---
+++ BizSecureDemo_22180023/ViewModels/RegisterVm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BizSecureDemo_22180023.ViewModels
+{
+    public class RegisterVm
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
+        [Required, MinLength(6)]
+        public string Password { get; set; } = "";
+    }
+}
